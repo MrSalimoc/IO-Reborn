b00d275d44e692a2af78471ac94910e464d1970f
MrSalimoc <mr.salimoc.minecraft@gmail.com>
2022-06-04 15:57:22 +0200

    Texture update (temporary)

--- src/main/java/mrsalimoc/ioreborn/common/peripherals/sensor/SensorModel.java
+++ src/main/java/mrsalimoc/ioreborn/common/peripherals/sensor/SensorModel.java
@@ -17,25 +17,25 @@ public class SensorModel extends Model {
     public SensorModel() {
         super(RenderType::entitySolid);
 
-        texWidth = 128;
-        texHeight = 128;
+        texWidth = 64;
+        texHeight = 64;
 
         bb_main = new ModelRenderer(this);
         bb_main.setPos(0.0F, 4.0F, 0.0F);
         bb_main.texOffs(0, 0).addBox(-0.5F, -8.0F, -0.5F, 1.0F, 4.0F, 1.0F, 0.0F, false);
-        bb_main.texOffs(0, 47).addBox(-3.5F, -4.0F, -0.5F, 7.0F, 6.0F, 1.0F, 0.0F, false);
+        bb_main.texOffs(0, 24).addBox(-3.5F, -4.0F, -0.5F, 7.0F, 6.0F, 1.0F, 0.0F, false);
 
         antenna_right_r1 = new ModelRenderer(this);
         antenna_right_r1.setPos(0.0F, 8.5F, 0.3465F);
         //bb_main.addChild(antenna_right_r1);
         setRotationAngle(antenna_right_r1, 0.0F, 0.3927F, 0.0F);
-        antenna_right_r1.texOffs(48, 47).addBox(-6.9488F, -8.5F, -2.1748F, 4.0F, 6.0F, 1.0F, 0.0F, true);
+        antenna_right_r1.texOffs(48, 24).addBox(-6.9488F, -8.5F, -2.1748F, 4.0F, 6.0F, 1.0F, 0.0F, true);
 
         antenna_left_r1 = new ModelRenderer(this);
         antenna_left_r1.setPos(0.0F, 8.5F, 0.3465F);
         //bb_main.addChild(antenna_left_r1);
         setRotationAngle(antenna_left_r1, 0.0F, -0.3927F, 0.0F);
-        antenna_left_r1.texOffs(32, 47).addBox(2.9488F, -8.5F, -2.1748F, 4.0F, 6.0F, 1.0F, 0.0F, false);
+        antenna_left_r1.texOffs(32, 24).addBox(2.9488F, -8.5F, -2.1748F, 4.0F, 6.0F, 1.0F, 0.0F, false);
 
     }
 
