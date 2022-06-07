07d6bc7448ee52caf6adc62d1095571b0ffc6ad8
MrSalimoc <mr.salimoc.minecraft@gmail.com>
2022-06-07 14:25:56 +0200

    Fix RFID Writer transparency render bug

--- src/main/java/mrsalimoc/ioreborn/client/ClientSetup.java
+++ src/main/java/mrsalimoc/ioreborn/client/ClientSetup.java
@@ -1,12 +1,16 @@
 package mrsalimoc.ioreborn.client;
 
 import mrsalimoc.ioreborn.IOReborn;
+import mrsalimoc.ioreborn.common.blocks.Blocks;
 import mrsalimoc.ioreborn.common.peripherals.energy_meter.EnergyMeterTileEntityRenderer;
 import mrsalimoc.ioreborn.common.peripherals.sensor.SensorTileEntityRenderer;
 import mrsalimoc.ioreborn.utils.Registration;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ForgeRenderTypes;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,6 +24,7 @@ public class ClientSetup {
     public static void init(final FMLClientSetupEvent event) {
         SensorTileEntityRenderer.register();
         EnergyMeterTileEntityRenderer.register();
+        RenderTypeLookup.setRenderLayer(Blocks.RFID_WRITER.get(), RenderType.translucent());
     }
 
     @SubscribeEvent
