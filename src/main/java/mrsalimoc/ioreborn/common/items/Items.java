b9dba6872993148ad75f2c494efc6ad0a6b76834
MrSalimoc <mr.salimoc.minecraft@gmail.com>
2022-06-09 15:30:35 +0200

    Update Items registry

--- src/main/java/mrsalimoc/ioreborn/common/items/Items.java
+++ src/main/java/mrsalimoc/ioreborn/common/items/Items.java
@@ -1,6 +1,8 @@
 package mrsalimoc.ioreborn.common.items;
 
 import mrsalimoc.ioreborn.IOReborn;
+import mrsalimoc.ioreborn.common.items.mag_card.MagCardItem;
+import mrsalimoc.ioreborn.common.items.silicon_ball.SiliconBallItem;
 import mrsalimoc.ioreborn.utils.Registration;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -8,6 +10,8 @@ import net.minecraftforge.fml.RegistryObject;
 public class Items {
 
     public static final RegistryObject<Item> PROXIMITY_CARD = Registration.ITEMS.register("proximity_card", () -> new ProximityCardItem(new Item.Properties().tab(IOReborn.IO_TAB)));
+    public static final RegistryObject<Item> SILICON_BALL = Registration.ITEMS.register("silicon_ball", () -> new SiliconBallItem(new Item.Properties().tab(IOReborn.IO_TAB)));
+    public static final RegistryObject<Item> MAG_CARD = Registration.ITEMS.register("mag_card", () -> new MagCardItem(new Item.Properties().tab(IOReborn.IO_TAB)));
 
     public static void register() {
 
